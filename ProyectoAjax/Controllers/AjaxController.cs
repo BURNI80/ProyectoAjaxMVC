03edc93ecfcc08dee2d1dbceb4b3a9f895211134
BURNI80 <alvarogutierrezc6@gmail.com>
2023-04-02 15:27:07 +0200

    PrioyectoTerminado

---
+++ ProyectoAjax/Controllers/AjaxController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ProyectoAjax.Controllers
+{
+    public class AjaxController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public int GetDoble(int numero)
+        {
+            var doble = numero * 2;
+            return doble;
+        }
+
+        public string LoadDartos()
+        {
+            string texto = "Esto es una peticion con ajax!!!";
+            return texto;
+        }
+
+        public IActionResult PostAjax()
+        {
+            return View();
+        }
+
+        public IActionResult GetAjax()
+        {
+            return View();
+        }
+
+
+    }
+}
